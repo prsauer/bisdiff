32fc051ec66687b220dd4b781c0a38820a56cbee
Paul Sauer <paulrsauer@gmail.com>
2022-03-20 16:42:14 -0400

    Mostly working BIS for hpri calculator

--- script/loadouts.ts
+++ script/loadouts.ts
@@ -16,6 +16,7 @@ type SlotName =
   | "FINGER";
 
 type Item = {
+  id: number;
   name: string;
   slot: SlotName;
   primary: number;
@@ -34,6 +35,7 @@ type Loadout = {
 type LoadoutTotal = {
   loadout: Loadout;
   primary: number;
+  stam: number;
   versa: number;
   haste: number;
   mastery: number;
@@ -55,26 +57,50 @@ const one_of_each = [
 const two_of_each = ["FINGER"];
 
 function verifyLoadout(loadout: Loadout): boolean {
+  // Apparatus: "SHOULDER","FINGER"
+  // Xanshi: "BACK","LEGS"
+  // Sephuz: "SHOULDER","CHEST","NECK"
+  const limitedLegendarySlots: SlotName[] = ["SHOULDER", "CHEST", "NECK"];
+
   let valid = true;
 
+  // Only one of each slot
   one_of_each.forEach((a) => {
     if (loadout.items.filter((i) => i.slot === a).length !== 1) {
       valid = false;
     }
   });
+  // except for rings, which require 2
   two_of_each.forEach((a) => {
     if (loadout.items.filter((i) => i.slot === a).length !== 2) {
       valid = false;
     }
   });
 
+  // Must have 4 piece set
   if (loadout.items.filter((i) => i.name === "SetPiece").length !== 4) {
     valid = false;
   }
 
+  // Must have 2 legendaries equipped
   if (loadout.items.filter((i) => i.name === "Leggo").length !== 2) {
     valid = false;
   }
+
+  // At least 1 lego is equipped in the limited slot (ie: not unity)
+  if (
+    loadout.items.filter(
+      (i) => limitedLegendarySlots.includes(i.slot) && i.name === "Leggo"
+    ).length === 0
+  ) {
+    valid = false;
+  }
+
+  // Max 1 crafted item
+  if (loadout.items.filter((i) => i.name.startsWith("Crafted")).length > 1) {
+    valid = false;
+  }
+
   return valid;
 }
 
@@ -86,6 +112,7 @@ function computeTotal(loadout: Loadout): LoadoutTotal {
   return {
     loadout,
     primary: sumField(loadout.items, "primary"),
+    stam: sumField(loadout.items, "stam"),
     versa: sumField(loadout.items, "versa"),
     haste: sumField(loadout.items, "haste"),
     mastery: sumField(loadout.items, "mastery"),
@@ -94,6 +121,7 @@ function computeTotal(loadout: Loadout): LoadoutTotal {
 }
 
 const itemLibrary: Item[] = [];
+let globalItemIdCounter = 0;
 
 function mkItem(
   name: string,
@@ -106,6 +134,7 @@ function mkItem(
   crit: number
 ): Item {
   const item = {
+    id: ++globalItemIdCounter,
     name,
     slot,
     stam,
@@ -152,7 +181,11 @@ const legoLegs = mkItem("Leggo", "LEGS", 149, 325, 94, 94, 0, 0);
 const legoFinger = mkItem("Leggo", "FINGER", 0, 183, 160, 160, 0, 0);
 
 //
-const craftedVersaHRing = mkItem("Crafted", "FINGER", 0, 130, 195, 78, 0, 0);
+const craftedVersaHRing = mkItem("Crafted-So", "FINGER", 0, 130, 195, 78, 0, 0);
+const craftedHasteVRing = mkItem("Crafted-Ox", "FINGER", 0, 130, 78, 195, 0, 0);
+
+const craftedVersaHNeck = mkItem("Crafted-So", "NECK", 0, 130, 195, 78, 0, 0);
+const craftedHasteVNeck = mkItem("Crafted-Ox", "NECK", 0, 130, 78, 195, 0, 0);
 //
 
 const loadouts: Loadout[] = [
@@ -198,51 +231,111 @@ loadouts.forEach((l) => {
   console.log(computeTotal(l));
 });
 
-let it = new Combinatorics.Combination(itemLibrary, 12);
-console.log(`${itemLibrary.length} items in library`);
-console.log(`${it.length} possible iterations`);
+const legendaryChoices = itemLibrary.filter((a) => a.name === "Leggo");
+const setPieceChoices = itemLibrary.filter((a) => a.name === "SetPiece");
+const normalChoices = itemLibrary.filter(
+  (a) => !["Leggo", "SetPiece"].includes(a.name)
+);
 
-const progressDivider = (it.length as number) / 10;
+const legIter = new Combinatorics.Combination(legendaryChoices, 2);
+console.log(`${legendaryChoices.length} choose 2 => ${legIter.length}`);
+
+const setIter = new Combinatorics.Combination(setPieceChoices, 4);
+console.log(`${setPieceChoices.length} choose 4 => ${setIter.length}`);
+
+const normalIter = new Combinatorics.Combination(normalChoices, 6);
+console.log(`${normalChoices.length} choose 6 => ${normalIter.length}`);
+
+const totalCombinations =
+  (legIter.length as number) *
+  (setIter.length as number) *
+  (normalIter.length as number);
+
+console.log(`${itemLibrary.length} items in library`);
+console.log(`${totalCombinations} possible iterations`);
+
+const progressDivider = totalCombinations / 10;
 
 const validSolns: LoadoutTotal[] = [];
 
-for (let i = 0; i < it.length; i++) {
-  if (i % progressDivider === 0) {
-    console.log(Math.floor(i / progressDivider));
+let counter = 0;
+
+for (let iLeg = 0; iLeg < legIter.length; iLeg++) {
+  for (let iSet = 0; iSet < setIter.length; iSet++) {
+    for (let iNorm = 0; iNorm < normalIter.length; iNorm++) {
+      counter++;
+      const curLoadout = [
+        ...(legIter.nth(iLeg) as Item[]),
+        ...(setIter.nth(iSet) as Item[]),
+        ...(normalIter.nth(iNorm) as Item[]),
+      ];
+      if (!verifyLoadout({ name: "", items: curLoadout })) {
+        continue;
+      }
+      validSolns.push(
+        computeTotal({
+          name: `iter-${counter}`,
+          items: curLoadout,
+        })
+      );
+    }
   }
-
-  const curLoadout = it.nth(i) as Item[];
-
-  if (!verifyLoadout({ name: "", items: curLoadout })) {
-    continue;
-  }
-
-  validSolns.push(
-    computeTotal({
-      name: `iter-${i}`,
-      items: curLoadout,
-    })
-  );
 }
 
 console.log(`${validSolns.length} valid solutions`);
 
-validSolns.sort((a, b) => b.haste - a.haste);
-fs.writeFileSync(
-  join(process.cwd(), "validSolnsByHaste.json"),
-  JSON.stringify(validSolns, null, 2)
-);
+function printLoadout(totals: LoadoutTotal | undefined) {
+  if (!totals) {
+    console.log("null build");
+    return;
+  }
+  let craftedItem = totals.loadout.items.find((a) =>
+    a.name.startsWith("Crafted")
+  );
+  const craftedString = craftedItem
+    ? `crafted=${craftedItem.slot}-${craftedItem.name}`
+    : "";
 
-validSolns.sort((a, b) => b.versa - a.versa);
-fs.writeFileSync(
-  join(process.cwd(), "validSolnsByVersa.json"),
-  JSON.stringify(validSolns, null, 2)
-);
+  console.log(
+    `${totals.loadout.name} Lego:${totals.loadout.items
+      .filter((i) => i.name === "Leggo")
+      .map((a) => `${a.slot}`)} Set:${totals.loadout.items
+      .filter((i) => i.name === "SetPiece")
+      .map((a) => `${a.slot}`)} Rings:${totals.loadout.items
+      .filter((i) => i.slot === "FINGER")
+      .map((a) => `${a.name}`)} versa=${totals.versa} haste=${
+      totals.haste
+    } ${craftedString} int=${totals.primary} stam=${totals.stam} mast=${
+      totals.mastery
+    } crit=${totals.crit}`
+  );
+}
 
-validSolns.sort((a, b) => b.haste + b.versa - (a.haste + a.versa));
+console.log("Sorted by Haste:");
+printLoadout(validSolns[0]);
+printLoadout(validSolns[1]);
+printLoadout(validSolns[2]);
+
+console.log("Sorted by Haste, min versa 1104");
+const filtByVersa = validSolns.filter((a) => a.versa > 1104);
+printLoadout(filtByVersa[0]);
+printLoadout(filtByVersa[1]);
+printLoadout(filtByVersa[2]);
+
+// Print Item library
+const libraryReport = itemLibrary.map((i) => {
+  const firstIndex = validSolns.findIndex((a) => {
+    return (
+      a.loadout.items.findIndex((l) => {
+        return l.id === i.id;
+      }) > -1
+    );
+  });
+  return `${i.id} ${i.name} ${i.slot} ${i.haste} ${firstIndex}`;
+});
 fs.writeFileSync(
-  join(process.cwd(), "validSolnsByHasteVersaTotal.json"),
-  JSON.stringify(validSolns, null, 2)
+  join(process.cwd(), "itemsReport.json"),
+  libraryReport.join("\r\n")
 );
 
 export {};
